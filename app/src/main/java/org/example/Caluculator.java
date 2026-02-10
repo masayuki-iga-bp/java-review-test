ff359204d00f21ce758f671bf3ed226d37007cbd
masayuki-iga-bp <Masayuki.Iga.bp@jp.nttdata.com>
2026-02-10 05:09:34 +0000

    割り算メソッド新規追加

---
+++ app/src/main/java/org/example/Caluculator.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Caluculator {
+    /**
+     * 割り算を行います.
+     * @param a 割られる数
+     * @param b 割る数
+     * @return 割り算の結果
+     */
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
