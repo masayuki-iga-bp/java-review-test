f32ec20f4e9dd6f7da7850011db84f836ad072f4
masayuki-iga-bp <Masayuki.Iga.bp@jp.nttdata.com>
2026-02-10 05:40:00 +0000

    足し算メソッド追加

--- app/src/main/java/org/example/Caluculator.java
+++ app/src/main/java/org/example/Caluculator.java
@@ -1,7 +1,22 @@
 package org.example;
 
 public class Caluculator {
+    /**
+     * 割り算を行います.
+     * @param a 割られる数
+     * @param b 割る数
+     * @return 割り算の結果
+     */
     public int divide(int a, int b) {
         return a / b;
+    }
+    /**
+     * 足し算を行います.
+     * @param a 足される数
+     * @param b 足す数
+     * @return 足し算の結果
+     */
+    public int add(int a, int b) {
+        return a + b;
     }
 }
