131d645e82698991170f53530255ba6b9b354c7f
masayuki-iga-bp <Masayuki.Iga.bp@jp.nttdata.com>
2026-02-16 06:37:21 +0000

    掛け算と引き算メソッドを追加

--- app/src/main/java/org/example/Caluculator.java
+++ app/src/main/java/org/example/Caluculator.java
@@ -19,4 +19,31 @@ public class Caluculator {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int multiply(int x, int y) {
+        int temp = 0;
+        int result1 = x;
+        int result2 = y;
+        temp = result1 * result2;
+        if (temp > 0) {
+            return temp;
+        } else if (temp < 0) {
+            return temp;
+        } else {
+            return temp;
+        }
+    }
+
+    public int subtract(int a, int b) {
+        int x = a;
+        int y = b;
+        int z = x - y;
+        if (z != 0) {
+            if (z > 100) {
+                return z - 10;
+            }
+            return z;
+        }
+        return 0;
+    }
 }
